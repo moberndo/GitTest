2f6d4a307286f6b9de2f4d3286b52b5f1ac273cc
Markus Oberndorfer <markusoberndorfer@M2AiRKUS.fritz.box>
2023-10-29 21:19:23 +0100

    Main file added.

---
+++ main.py
@@ -0,0 +1,9 @@
+# title
+
+if __name__ == '__main__':
+    try:
+        import numpy as np
+    except:
+        raise(ImportError('Could not import Numpy.'))
+    else:
+        print('Numpy successfully imported.')
